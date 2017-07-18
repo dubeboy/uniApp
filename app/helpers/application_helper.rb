0da1bfacfccd5b3dc796544418c8abf0d3aa6977
dubeboy <216004854@student.uj.ac.za>
2017-07-18 16:30:42 +0200

    fix login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
       @current_user ||= JobSeeker.find_by(id: session[:user_id])
     end
   end
+
+  def get_name(current_user)
+    unless current_user.nil?
+      if current_user.instance_of? Company
+        return current_user.c_name
+      else
+        return current_user.j_username
+      end
+    end
+  end
 end
